9ed8ea7501bb1a2694e28d84836a87c17264299e
Cihan Güllü <cihangullu54@gmail.com>
2019-05-21 23:25:10 +0300

    registered interceptors

--- InterceptorDemo.Core/CoreModule.cs
+++ InterceptorDemo.Core/CoreModule.cs
@@ -1,9 +1,15 @@
 ﻿using Autofac;
+using InterceptorDemo.Core.Aspects.CastleDynamicProxy.ExceptionAspects;
+using InterceptorDemo.Core.Aspects.CastleDynamicProxy.LogAspects;
 
 namespace InterceptorDemo.Core
 {
 	public class CoreModule : Module
 	{
-		protected override void Load(ContainerBuilder builder) { }
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<ExceptionHandlingInterceptor>();
+			builder.RegisterType<LogInterceptor>();
+		}
 	}
 }
