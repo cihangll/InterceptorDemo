eed366423d3d5bf8b3e5c0e39ac978131ef81bc2
Cihan Güllü <cihangullu54@gmail.com>
2019-05-21 22:38:46 +0300

    added ExceptionAttribute

---
+++ InterceptorDemo.Core/Aspects/CastleDynamicProxy/ExceptionAspects/ExceptionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InterceptorDemo.Core.Aspects.CastleDynamicProxy.ExceptionAspects
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class ExceptionAttribute : Attribute
+	{
+		public bool IsActive { get; private set; }
+
+		public ExceptionAttribute(bool isActive = true)
+		{
+			IsActive = isActive;
+		}
+	}
+}
